bd6effb6e31ac6e7946ec43f3a41b495fd8fcdb6
Thitipong Samranvanich <tpsxx2015@gmail.com>
2020-11-30 20:16:47 +0700

    add how to run

--- index.php
+++ index.php
@@ -5,3 +5,11 @@
 <li> <a href="sec-1-id.php"> sec-1-id</a> </li> 
 <li><a href="detail01.php"> detail01 </a></li> 
 </ul>
+
+<pre> 
+    run with 
+    c:\xxxxxx\php.exe  -S localhost:82
+    
+    browse with 
+    http://localhost:82/
+</pre> 
