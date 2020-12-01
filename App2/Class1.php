8025d5917618ff1261d5cbbf1a156b437f1e4d8f
Thitipong Samranvanich <tpsxx2015@gmail.com>
2020-12-02 05:19:00 +0700

    add Lab1 branch , for test [PDO_SQLite,PHP Namespace]

---
+++ App2/Class1.php
@@ -0,0 +1,8 @@
+<?php
+namespace App2 ;
+
+class Class1{
+    const INFO = "Class1";
+}
+
+echo Class1::INFO;
---
+++ App2/Class2.php
@@ -0,0 +1,10 @@
+<?php
+namespace App2 ;
+// include("Class1.php");
+
+class Class2{
+    const INFO = "Class2";
+}
+
+echo Class1::INFO; 
+echo Class2::INFO;
