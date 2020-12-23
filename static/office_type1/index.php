4944852c531033ec1eea4f03beb96247ae8f6b0d
Thitipong Samranvanich <tpsxx2015@gmail.com>
2020-12-23 23:13:32 +0700

    add company policy type1

---
+++ static/office_type1/index.php
@@ -0,0 +1,22 @@
+
+
+การรักษาความมั่นคงปลอดภัยด้านเทคโนโลยีสารสนเทศ (IT security) 
+<br>
+สำหรับหน่วยงานแบบที่ 1 
+
+<ul>
+    <li><a href="office_type1/it_security_01_05.php" id="ref">1-5. System Developement</a></li>
+    <li><a href="office_type1/it_security_06.php" id="ref">6. IT operations security </a></li>
+    <li><a href="office_type1/it_07_system_dev.php" id="ref">7. System Developement</a></li>
+    <li><a href="office_type1/it_08_incident_mng.php" id="ref">8. Incident Management</a></li>
+</ul>
+
+<pre>
+
+อ้างอิงจาก  REF : 
+https://www.bot.or.th/Thai/FIPCS/Documents/FOG/2562/ThaiPDF/25620272.pdf
+
+
+</pre>
+ 
+
